51d8064f00bc00715976448a8ff6016289addbfb
Jorrit Geurts <r0956231@student.thomasmore.be>
2024-11-11 11:39:37 +0100

    Add all Files

---
+++ Belgische-regering/politieke-paritijen/src/main/java/fact/it/politiekeparitijen/PolitiekeParitijenApplication.java
@@ -0,0 +1,13 @@
+package fact.it.politiekeparitijen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolitiekeParitijenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolitiekeParitijenApplication.class, args);
+	}
+
+}
---
+++ Belgische-regering/politieke-paritijen/src/test/java/fact/it/politiekeparitijen/PolitiekeParitijenApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.politiekeparitijen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolitiekeParitijenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
