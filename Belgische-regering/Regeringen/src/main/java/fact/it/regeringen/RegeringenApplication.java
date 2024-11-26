e5e05f667b25e612c16c090ace6f98dcebeca970
Jorrit Geurts <r0956231@student.thomasmore.be>
2024-11-26 13:15:19 +0100

    first commit Regeringen

---
+++ Belgische-regering/Regeringen/src/main/java/fact/it/regeringen/RegeringenApplication.java
@@ -0,0 +1,13 @@
+package fact.it.regeringen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegeringenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegeringenApplication.class, args);
+	}
+
+}
