88e96ec02473efbf0545f6fe82c5dad2e5bbbd6e
Arno Van haecke <r0929451@student.thomasmore.be>
2024-12-03 14:36:09 +0100

    Begin partijlid-service

---
+++ Belgische-regering/partijlid-service/src/main/java/com/example/partijlidservice/PartijlidServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.partijlidservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartijlidServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartijlidServiceApplication.class, args);
+	}
+
+}
