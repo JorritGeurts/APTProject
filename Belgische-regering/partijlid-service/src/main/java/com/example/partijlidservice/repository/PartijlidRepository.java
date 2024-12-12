2495c397ebb0253a2caf4abdeb848dd434e2e364
Arno Van haecke <113921342+Van-haecke-Arno@users.noreply.github.com>
2024-12-12 12:30:29 +0100

    Delete Belgische-regering/partijlid-service directory

--- Belgische-regering/partijlid-service/src/main/java/com/example/partijlidservice/repository/PartijlidRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.partijlidservice.repository;
-
-import com.example.partijlidservice.model.Partijlid;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PartijlidRepository extends JpaRepository<Partijlid, Long> {
-}
