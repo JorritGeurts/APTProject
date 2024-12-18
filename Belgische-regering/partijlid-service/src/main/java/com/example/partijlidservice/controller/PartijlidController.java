a17a76c2ed219e4340667a3c91c72f45a6727cda
Arno Van haecke <r0929451@student.thomasmore.be>
2024-12-18 13:18:58 +0100

    CORS

--- Belgische-regering/partijlid-service/src/main/java/com/example/partijlidservice/controller/PartijlidController.java
+++ Belgische-regering/partijlid-service/src/main/java/com/example/partijlidservice/controller/PartijlidController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/partijlid")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class PartijlidController {
 
     private final PartijlidService partijlidService;
