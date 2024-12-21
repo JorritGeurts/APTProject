3ed3c76c33943db74dd0397055d9a3d3ad005770
Jorrit Geurts <r0956231@student.thomasmore.be>
2024-12-21 15:15:44 +0100

    klaar

--- Belgische-regering/api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
+++ Belgische-regering/api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                         return config;
                     }))
                 .authorizeExchange(exchange ->
-                        exchange.pathMatchers(HttpMethod.GET, "/alle-partijen", "/partij/{naam}", "/alle-regeringen", "/regering/{naam}", "/partijleden", "/minister/{segment}").permitAll()
+                        exchange.pathMatchers(HttpMethod.GET, "/alle-partijen", "/partij/{naam}", "/alle-regeringen", "/regering/{naam}", "/partijleden","/partijlid/{id}", "/ministers", "/minister/{id}").permitAll()
                                 .anyExchange().authenticated())
 
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
