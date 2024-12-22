1d4fcaef33549eaa66cc1ba2e47d57c42afb4c87
Jorrit Geurts <r0956231@student.thomasmore.be>
2024-12-22 14:39:08 +0100

    final push

--- belgische-regering-application/src/app/services/minister.service.ts
+++ belgische-regering-application/src/app/services/minister.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { Minister } from '../interfaces/minister';
 import { Observable } from 'rxjs';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+import { AuthGoogleService } from './auth-google.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MinisterService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private authService: AuthGoogleService) { }
   private apiUrl = "http://localhost:8083"; 
 
 
@@ -25,19 +26,45 @@ export class MinisterService {
   }
 
   postMinister(minister: Minister): Observable<Minister> {
-    let headers = new HttpHeaders();
-    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+    const token = this.authService.getIdToken();
+    
+    if (token) {
+      const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`
+      });
     return this.httpClient.post<Minister>(`${this.apiUrl}/minister/create`, minister, {headers: headers, });
+    } else {
+      console.error('No token found');
+      return new Observable(); 
+    }
   }
 
   putMinister(id: number, minister: Minister): Observable<Minister> {
-    let headers = new HttpHeaders();
-    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+    const token = this.authService.getIdToken();
+    
+    if (token) {
+      const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`
+      });
     return this.httpClient.put<Minister>(`${this.apiUrl}/minister/${id}/edit`, minister, {headers: headers, });
+  } else {
+    console.error('No token found');
+    return new Observable(); 
+  }
 
   }
 
   deleteMinister(id: number): Observable<Minister> {
-    return this.httpClient.delete<Minister>(`${this.apiUrl}/minister/${id}/delete`);
+    const token = this.authService.getIdToken();
+    
+    if (token) {
+      const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`
+      });
+    return this.httpClient.delete<Minister>(`${this.apiUrl}/minister/${id}/delete`, {headers: headers, });
+  } else {
+    console.error('No token found');
+    return new Observable(); 
+  }
   }
 }
